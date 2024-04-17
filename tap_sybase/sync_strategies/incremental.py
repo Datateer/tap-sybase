c858c3c9603115f53b9d362d822b483ed1735902
Alexander Shea <44410965+frctnlss@users.noreply.github.com>
2024-04-17 12:36:03 -0600

    format datetime to milliseconds instead of microseconds

--- tap_sybase/sync_strategies/incremental.py
+++ tap_sybase/sync_strategies/incremental.py
@@ -54,7 +54,7 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns):
             if replication_key_value is not None:
                 if catalog_entry.schema.properties[replication_key_metadata].format == "date-time":
                     replication_key_value = pendulum.parse(replication_key_value)
-                    replication_key_value = replication_key_value.strftime('%Y-%m-%d %H:%M:%S.%f')
+                    replication_key_value = replication_key_value.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
                 select_sql += " WHERE \"{}\" >= %(replication_key_value)s ORDER BY \"{}\" ASC".format(
                     replication_key_metadata, replication_key_metadata
